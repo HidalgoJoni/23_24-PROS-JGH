2f82df6c4567581c9f95172dc07acc508808b876
HidalgoJoni <jonigarciah@gmail.com>
2023-10-10 10:50:26 +0200

    UD2.ACT4-5.intento1.definitivo
    Realizacion actividades 4 y 5 en el primer intento definitivo

---
+++ UD2/ACT4-5/code/Primero.java
@@ -0,0 +1,17 @@
+package ejer5;
+
+// Clase Primero que extiende la clase Thread
+class Primero extends Thread {
+    public void run() {
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Primero " + i);
+
+            try {
+                // Dormir durante 100 milisegundos
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
