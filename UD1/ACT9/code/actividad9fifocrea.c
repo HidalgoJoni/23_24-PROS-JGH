cfe7d33dabfa17a1670dc612af8abdd8ff705b94
HidalgoJoni <jonigarciah@gmail.com>
2023-10-27 09:22:59 +0200

    UD1.ACT9.intento2.definitivo

--- UD1/ACT9/code/actividad9fifocrea.c
+++ UD1/ACT9/code/actividad9fifocrea.c
@@ -2,34 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
-int main (void) {
+int main(void) {
     int fp;
-    int p, bytesleidos;
-    char buffer [100]; // Aumentamos el tamaño del buffer
-
-    p = mknod("FIF02", S_IFIFO|0666, 0);
-
-    if (p == -1) {
-        printf("Ha ocurrido un error.... \n");
-        exit (0);
-    }
+    int bytesleidos;
+    char buffer[100]; // Aumentamos el tamaño del buffer
 
     while (1) {
-        fp = open ("FIF02", O_RDONLY); // Cambiamos el modo a lectura solamente
-        bytesleidos = read(fp, buffer, sizeof(buffer)); // Leemos más bytes
-        printf("Obteniendo información...\n");
+        fp = open("FIF02", O_RDONLY); // Cambiamos el modo a lectura solamente
 
-        while (bytesleidos > 0) {
-            printf("%.*s", bytesleidos, buffer); // Utilizamos %.*s para imprimir solo la cantidad de bytes leídos
-            bytesleidos = read (fp, buffer, sizeof(buffer)); // leemos más bytes
+        if (fp == -1) {
+            printf("Error al abrir el fichero... \n");
+            exit(1);
         }
 
-        close (fp);
+        printf("Obteniendo información...\n");
+
+        while ((bytesleidos = read(fp, buffer, sizeof(buffer))) > 0) {
+            printf("%.*s", bytesleidos, buffer); // Utilizamos %.*s para imprimir solo la cantidad de bytes leídos
+        }
+
+        close(fp);
     }
 
-    return(0);
+    return 0;
 }
--- UD1/ACT9/code/actividad9fifoescribe.c
+++ UD1/ACT9/code/actividad9fifoescribe.c
@@ -2,14 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 int main(void) {
     int fp;
     char saludo[] = "Un saludo !!!!!\n";
-    
-    fp = open("FIFO2", O_WRONLY); 
+
+    // Crear el FIFO solo si no existe
+    if (access("FIF02", F_OK) == -1) {  // Verificar si el archivo no existe
+        if (mknod("FIF02", S_IFIFO | 0666, 0) == -1) {
+            perror("Error al crear el FIFO");
+            exit(0);
+        }
+    }
+
+    fp = open("FIF02", O_WRONLY);
 
     if (fp == -1) {
         printf("Error al abrir el fichero... \n");
@@ -18,7 +28,7 @@ int main(void) {
 
     printf("Mandando información al FIFO...\n");
 
-    write(fp, saludo, strlen(saludo)); 
+    write(fp, saludo, strlen(saludo));
     close(fp);
 
     return 0;
