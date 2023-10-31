5ef69d31979123f5d91ea81af6a72b71a0613bdc
HidalgoJoni <jonigarciah@gmail.com>
2023-10-31 12:50:01 +0100

    UD1.ACT8.intento2_v2.definitivo

--- UD1/ACT8/code/actividad8.c
+++ UD1/ACT8/code/actividad8.c
@@ -1,80 +1,98 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
-int main() {
-    int fd1[2], fd2[2];
-    pid_t p;
+void main ()
+{ 
+    int fd1[2];
+    char buffer[80];
+    char saludoAbuelo[]="Saludo del ABUELO..";
+    pid_t pid;
+    pipe(fd1); // Se crea el pipe o tubería
+    pid = fork();
+    switch (pid) {
+        case -1: // error
+        printf("No se ha podido crear el proceso hijo...\n");
+        exit (-1);
+        break;
+        case 0:   // Hijo recibe
+        close(fd1[1]); // Cierra el descriptor de escritura
+        read (fd1[0], buffer, sizeof(buffer)); // leo el pipe
+        printf ("\tEl Hijo recibe mensaje de ABUELO: %s\n", buffer);
+        // crear el mensaje para el nieto
+        int fd2[2];
+        char saludoPadre[]="Saludo del PADRE..";
 
-    // Crear los pipes
-    if (pipe(fd1) == -1) {
-        fprintf(stderr, "Pipe Failed");
-        return 1;
-    }
-    if (pipe(fd2) == -1) {
-        fprintf(stderr, "Pipe Failed");
-        return 1;
-    }
-
-    p = fork();
-
-    // Proceso abuelo
-    if (p > 0) {
-        char buff[100];
-
-        close(fd1[0]);  // Cerrar el extremo de lectura del primer pipe
-        write(fd1[1], "Saludo del abuelo", strlen("Saludo del abuelo") + 1);
-        printf("El ABUELO envía un mensaje al HIJO...\n");
-        close(fd1[1]);
-
-        wait(NULL);  // Esperar a que el hijo termine
-
-        close(fd2[1]); // Cerrar el extremo de escritura del segundo pipe
-        read(fd2[0], buff, sizeof(buff));
-        printf("El ABUELO recibe mensaje del HIJO: %s\n", buff);
-        close(fd2[0]);
-    }
-    // Proceso hijo
-    else {
-        pid_t p2 = fork();
-
-        if (p2 > 0) {
-            char buff[100];
-
-            close(fd1[1]);  // Cerrar el extremo de escritura del primer pipe
-            read(fd1[0], buff, sizeof(buff));
-            printf("\tEl HIJO recibe mensaje del ABUELO: %s\n", buff);
-            close(fd1[0]);
-
-            close(fd2[0]);  // Cerrar el extremo de lectura del segundo pipe
-            write(fd2[1], "Saludo del padre", strlen("Saludo del padre") + 1);
-            printf("\tEl HIJO envía un mensaje al NIETO...\n");
-
-            wait(NULL);  // Esperar a que el nieto termine
-
-            read(fd1[0], buff, sizeof(buff));
-            printf("\tEl HIJO recibe mensaje de su hijo: %s\n", buff);
-            close(fd1[0]);
-
-            write(fd2[1], "Saludo del hijo", strlen("Saludo del hijo") + 1);
-            printf("\tEl HIJO envía un mensaje al ABUELO...\n");
-            close(fd2[1]);
+        char saludoHijo[]="Saludo del HIJO..";
+        pipe(fd2); // Se crea el pipe o tubería
+        pid = fork();
+            switch (pid) {
+                case -1: // error
+                printf("No se ha podido crear el proceso nieto...\n");
+                exit (-1);
+                break;
+                case 0:   // Nieto recibe
+                close(fd2[1]); // Cierra el descriptor de escritura
+                read (fd2[0], buffer, sizeof(buffer)); // leo el pipe
+                printf ("\t\tEl NIETO recibe mensaje del PADRE: %s\n", buffer);
+                // crear el mensaje para el hijo
+                char saludoNieto[]="Saludo del NIETO..";
+                pipe(fd1); // Se crea el pipe o tubería
+                pid = fork();
+                    switch (pid) {
+                        case -1: // error
+                            printf("No se ha podido crear el proceso hijo...\n");
+                            exit (-1);
+                            break;
+                            case 0:   // Hijo recibe
+                            close(fd1[1]); // Cierra el descriptor de escritura
+                            read (fd1[0], buffer, sizeof(buffer)); // leo el pipe
+                            printf ("\tEl HIJO recibe mensaje de SU HIJO: %s\n", buffer);
+                            // crear el mensaje para el abuelo
+                            pipe(fd2); // Se crea el pipe o tubería
+                            pid = fork();
+                                switch (pid) {
+                                    case -1: // error
+                                        printf("No se ha podido crear el proceso hijo...\n");
+                                        exit (-1);
+                                        break;
+                                        case 0:   // Hijo recibe
+                                        close(fd2[1]); // Cierra el descriptor de escritura
+                                        read (fd2[0], buffer, sizeof(buffer)); // leo el pipe
+                                        printf ("El ABUELO lee el mensaje del HIJO: %s\n", buffer);
+                                    break;
+                                    default: // Hijo envia a Abuelo
+                                        close(fd2[0]); // Cierra el descriptor de lectura
+                                        write (fd2[1], saludoHijo, sizeof(saludoHijo));
+                                        printf ("\tEl HIJO envía un mensaje al ABUELO...\n");
+                                        wait (NULL); // Espera al proceso hijo
+                                        break;
+                                    }
+                        break;
+                        default: // Nieto envia a hijo
+                            close(fd1[0]); // Cierra el descriptor de lectura
+                            write (fd1[1], saludoNieto, sizeof(saludoNieto));
+                            printf ("\t\tEl NIETO envía un mensaje al HIJO...\n");
+                            wait (NULL); // Espera al proceso hijo
+                            break;
+                        }
+                break;
+                default: // Padre Envía a nieto
+                    close(fd2[0]); // Cierra el descriptor de lectura
+                    write (fd2[1], saludoPadre, sizeof(saludoPadre));
+                    printf ("\tEl HIJO envía un mensaje al NIETO...\n");
+                    wait (NULL); // Espera al proceso hijo
+                    break;
+                }
+        break;
+        default: // Abuelo Envía
+            close(fd1[0]); // Cierra el descriptor de lectura
+            write (fd1[1], saludoAbuelo, sizeof(saludoAbuelo));
+            printf ("El ABUELO envía un mensaje al HIJO...\n");
+            wait (NULL); // Espera al proceso hijo
+            break;
         }
-        // Proceso nieto
-        else {
-            char buff[100];
-
-            close(fd2[1]);  // Cerrar el extremo de escritura del segundo pipe
-            read(fd2[0], buff, sizeof(buff));
-            printf("\t\tEl NIETO recibe mensaje del PADRE: %s\n", buff);
-            close(fd2[0]);
-
-            write(fd1[1], "Saludo del nieto", strlen("Saludo del nieto") + 1);
-            printf("\t\tEl NIETO envía un mensaje al HIJO...\n");
-            close(fd1[1]);
-        }
-    }
-
-    return 0;
+    exit(0);
 }
+
